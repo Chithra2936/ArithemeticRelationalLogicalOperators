c71873914665da79292c098e085a6cb72989d584
Chithra2936 <kurmachithrareddy2936@gmail.com>
2025-01-28 17:14:05 +0530

    Add files via upload

---
+++ Relational.java
@@ -0,0 +1,30 @@
+public class Relational {
+    public static void main(String[] args) {
+       
+        int num1 = 10;
+        int num2 = 3;
+
+        System.out.println("Arithmetic Operators:");
+        System.out.println("num1 + num2 = " + (num1 + num2));
+        System.out.println("num1 - num2 = " + (num1 - num2));
+        System.out.println("num1 * num2 = " + (num1 * num2));
+        System.out.println("num1 / num2 = " + (num1 / num2));
+        System.out.println("num1 % num2 = " + (num1 % num2));
+
+        // Relational Operators
+        System.out.println("\nRelational Operators:");
+        System.out.println("num1 == num2 = " + (num1 == num2));
+        System.out.println("num1 != num2 = " + (num1 != num2));
+        System.out.println("num1 > num2 = " + (num1 > num2));
+        System.out.println("num1 < num2 = " + (num1 < num2));
+        System.out.println("num1 >= num2 = " + (num1 >= num2));
+        System.out.println("num1 <= num2 = " + (num1 <= num2));
+
+        // Logical Operators
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println("\nLogical Operators:");
+        System.out.println("bool1 && bool2 = " + (bool1 && bool2));
+    }
+}
